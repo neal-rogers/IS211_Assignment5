860b52ce6fb4a86b3001cb770e626354ddab1bf9
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-03-03 13:30:56 -0500

    Added printer.py class file, test program to retrieve assignment data (getdatafromurl.py), and added required functions to simulation.py

---
+++ getdatafromurl.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This program does stuff."""
+
+
+import urllib2
+
+
+def downloadData(url):
+    response = urllib2.urlopen(url)
+    data = response.read()
+    return data
+
+
+def saveData(file_data):
+    input_file = file_data.split()
+    output_file = open("data.txt", "w")
+
+    for line in input_file:
+        output_file.write(line)
+        output_file.write("\n")
+
+    output_file.close()
+    print 'Data saved to file. Exiting program...'
+
+
+def main():
+    url = 'http://s3.amazonaws.com/cuny-is211-spring2015/requests.csv'
+    data = downloadData(url)
+    result = saveData(data)
+
+
+if __name__ == '__main__':
+    main()
--- simulation.py
+++ simulation.py
@@ -3,4 +3,21 @@
 """This program DOES SOME STUFF.
 """
 
+from printer import Printer
+from server import Server
 
+
+def simulateOneServer(input_file):
+    pass
+
+
+def simulateManyServers():
+    return avg_latency
+
+
+def main():
+    simulateOneServer(input_file)
+    pass
+
+if __name__ == '__main__':
+    main()
